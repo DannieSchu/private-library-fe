ca2384047529f7d0fe70aa08a42cbdb5934e666a
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-14 00:58:10 -0700

    write failing reducer test for the SET_BOOKS action

--- src/reducers/booksReducerTest.js
+++ src/reducers/booksReducerTest.js
@@ -1,4 +1,4 @@
-import { addbook } from '../actions/booksActions';
+import { addbook, setBooks } from '../actions/booksActions';
 import reducer from './booksReducer';
 
 describe('books reducer', () => {
@@ -18,6 +18,27 @@ describe('books reducer', () => {
       _id: 'f4fvfwev',
       title: 'Mansfield Park',
       authorId: 'fwegew09vjos',
+      genre: 'literature',
+      pages: 325
+    }]);
+  });
+
+  it('handles the SET_BOOKS action', () => {
+    const state = [];
+    const action = setBooks([{
+      _id: 'f4fvfwev',
+      title: 'Mansfield Park',
+      author: 'Jane Austen',
+      genre: 'literature',
+      pages: 325    
+    }]);
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([{
+      _id: 'f4fvfwev',
+      title: 'Mansfield Park',
+      author: 'Jane Austen',
       genre: 'literature',
       pages: 325
     }]);
