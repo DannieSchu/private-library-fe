7ef828f2615b4d8af0a705c53682da7310f7ca52
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-14 01:30:24 -0700

    render Bookshelf list component

--- src/components/Bookshelf/Book.jsx
+++ src/components/Bookshelf/Book.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ title, genre, pages }) => (
+const Book = ({ title, author, genre, pages }) => (
   <section>
     <h3>{title}</h3>
+    <p>Author: {author}</p>
     <p>Genre: {genre}</p>
     <p>Number of pages: {pages}</p>
   </section>
@@ -11,6 +12,7 @@ const Book = ({ title, genre, pages }) => (
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   pages: PropTypes.number.isRequired,
 };
--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Book from './Book';
+import { fetchBooks } from '../../services/bookAPI';
+import { setBooks } from '../../actions/booksActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { getBooks } from '../../selectors/booksSelectors';
 
 const Bookshelf = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchBooks()
+      .then(books => {
+        dispatch(setBooks(books));
+      });
+  }, []);
+
+  const books = useSelector(getBooks);
+
+  const bookElements = books.map(book => (
+    <li key={book._id}>
+      <Book {...book} />
+    </li>)
+  );
+  
   return (
-    <>
-    </>
+    <ul>
+      {bookElements}
+    </ul>
   );
 };
 
