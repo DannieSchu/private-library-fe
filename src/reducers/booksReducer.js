4093ec953fe2a5513d9b8ef1b0152eabe7e83e20
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-14 00:59:04 -0700

    booksReducer passes test

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,9 +1,11 @@
-import { ADD_BOOK } from '../actions/booksActions'; 
+import { ADD_BOOK, SET_BOOKS } from '../actions/booksActions'; 
 
 export default function reducer(state = [], action) {
   switch(action.type) {
     case ADD_BOOK:
       return [...state, action.payload]; 
+    case SET_BOOKS:
+      return action.payload;
     default:
       return state;
   }
