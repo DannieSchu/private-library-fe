da77e4758862b5b0e1ff070c9bd63fba7c39e2a1
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-13 18:45:33 -0700

    create router in App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+import Bookshelf from '../Bookshelf/Bookshelf';
+import BookForm from '../BookForm/BookForm';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/shelf" component={Bookshelf} />
+        <Route path="/form" component={BookForm} />
+      </Switch>
+    </Router>
+  );
 }
-  
